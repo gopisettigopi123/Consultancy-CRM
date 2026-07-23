9e64fa0ea71c1d46bda439c58420f6f8038d9b12
GOPISETTI GOPI <gopisettigopi6756@gmail.com>
2026-07-24 00:56:56 +0530

    feat:for deployment purpose added frotnedn url index.js

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ const app = express();
 app.use(express.json());
 // app.use(cors());
 app.use(cors({
-    origin: ['http://localhost:5173', 'https://crm-frontend.vercel.app'], // Add your Vercel URL here
+    origin: ['http://localhost:5173', 'https://consultancy-crm-omega.vercel.app/'], // Add your Vercel URL here
     credentials: true
 }));
 
